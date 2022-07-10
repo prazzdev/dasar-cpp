33d90d0c59ad4099d313c2f370ecfefae34debf7
Agung Prasetyo <prazzid31@gmail.com>
2022-07-10 23:58:17 +0700

    Add files via upload

---
+++ KARAKTER.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char huruf, simbol, digit;
+	huruf	= 'A';
+	simbol	= '+';
+	digit	= '4';
+	cout<<huruf<<" "<<simbol<<" "<<digit<<" "<<endl;
+	system("pause");
+}
---
+++ LOOPING WHILE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+	int main(){
+		cout<<"LOOPING WHILE\n\n";
+		int I=1;
+		while (I<=5){
+			I++;
+			cout<<I<<endl;
+			}
+			system("pause");
+		}
