c681b9a76aaec531b30d13ec15bcabd72c1c5711
Agung Prasetyo <prazzid31@gmail.com>
2022-07-10 23:55:20 +0700

    boolean cpp

---
+++ assets/BOOLEAN.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	bool pria;
+	pria=true;
+	if(true)
+		cout<<"Pria"<<endl;
+	else
+		cout<<"Perempuan"<<endl;
+	system("pause");
+}
---
+++ assets/Yes or No.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ulangi = '.';
+    int counter = 0;
+
+    // perulangan while
+    while(ulangi == '.'){
+        printf("uh239u112yd93q8d0jr84f-jtqu30jfp0ta93utf03yt9fh9uho9a9fjpt49ag0tpugtufkat8hy835haf8h3a805pg0j9uf5py5a88");
+        printf("rotei4[9tw4ugh5407ga89yr4f8ya9tr4dypar049d4pya0tdy40]");
+        cin >> ulangi;
+
+        // increment counter
+        counter++;
+    }
+
+    printf("\n\n----------\n");
+    printf("Perulangan Selesai!\n");
+    printf("Kamu mengulang sebanyak %i kali.\n", counter);
+
+    return 0;
+}
